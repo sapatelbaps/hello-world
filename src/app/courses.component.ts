bb12a52c93f7af588479655d4f2c6ad35ab3d073
sapatelbaps <sapatelbaps@gmail.com>
2019-02-23 21:50:44 -0800

    Directives added. TO display multiple courses from array.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', // <courses></courses>
   //template: '<h2>{{ "Title: " + title }}</h2>'
-  template: '<h2>{{ getTitle() }}</h2>'
+  template: `
+  <h2>{{ getTitle() }}</h2>
+  <ul>
+    <li *ngFor="let course of courses">
+      {{course}}
+    </li>
+  </ul>
+  `
 })
 export class CoursesComponent{
   title = "List of courses";
+  courses = ["course 1" , "course 2", "course 3"]
 
-  getTitle() {
-    return this.title;
-  }
 }
